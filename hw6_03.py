6911fa7466851708ce2ecaeae91746c4be964a0c
paha85 <padisb@gmail.com>
2022-04-13 22:41:32 +0300

    дз6

--- hw6_03.py
+++ hw6_03.py
@@ -13,14 +13,10 @@ with open('hobby.csv', 'r', encoding='utf-8') as f_hobby:
         hobbies.append(i.replace('\n', ' ').replace(',', ', '))
     print(hobbies)
 
-data = dict(zip_longest(names, hobbies))
-for k, v in data.items():
-    if k == None:
-        sys.exit('1')
-    else:
-        print("Resultant dictionary is : " + str(data))
-
-
-
 with open('list.txt', 'w', encoding='utf-8') as name_hobby:
-    name_hobby.write(str(data))
+    data = dict(zip_longest(names, hobbies))
+    for k, v in data.items():
+        if k == None:
+            sys.exit('1')
+    else:
+        name_hobby.write(str(data))
